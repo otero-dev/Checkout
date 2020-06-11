8100706e949931991a3e77e02e4b03b8eab9239f
web-co-dev <brightstar.soft@hotmail.com>
2020-06-11 09:17:31 -0400

    Add limit

--- pages/api/metafields.js
+++ pages/api/metafields.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default (req, res) => {
     const { productId } = req.body;
-    axios.get(`https://${process.env.API_KEY}:${process.env.SHARED_SECRET}@${process.env.STORE_URL}/admin/products/${productId}/metafields.json`)
+    axios.get(`https://${process.env.API_KEY}:${process.env.SHARED_SECRET}@${process.env.STORE_URL}/admin/products/${productId}/metafields.json?limit=250`)
       .then(response => {
         const metadata = response.data.metafields.filter(field => field.namespace === 'Ultra')
         res.send(metadata);
