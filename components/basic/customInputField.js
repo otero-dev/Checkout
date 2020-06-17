794eb5ccd41eab30c368d9acf925eafd445f5738
web-co-dev <brightstar.soft@hotmail.com>
2020-06-17 16:11:45 -0400

    Update email regex to validate special sign before @ sign

--- components/basic/customInputField.js
+++ components/basic/customInputField.js
@@ -26,7 +26,7 @@ const Input = (props) => {
   const checkValidation = (ev) => {
     setEdit(false);
     if (props.name === "email") {
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(ev))
+      if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(ev))
         setValid(true);
       else {
         setValid(false);
